59d82b17e016556990b32dec55ee8c8ab0a00ee9
ben.andrew <ben.andrew@SYSTEM179>
2024-05-01 14:47:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AbsoluteUnit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
