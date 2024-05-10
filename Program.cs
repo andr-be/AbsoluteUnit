1b1948e396790131a4f104507cca76c9c5ac0715
andr-be <andr-be@github.com>
2024-05-10 15:16:45 +0100

    minor program changes for better debug output

--- Program.cs
+++ Program.cs
@@ -7,8 +7,14 @@
             try
             {
                 CommandParser commandParser = new(args);
-                Console.WriteLine($"{commandParser.CommandType} mode selected!");
-                for (int i = 1; i < args.Length; i++) Console.WriteLine($"arg [{i}]: {args[i]}");
+                
+                Console.WriteLine($"Command: {commandParser.CommandType}\n");
+                int i = 1; foreach (var arg in commandParser.CommandArguments) 
+                    Console.WriteLine($"{i++}: {arg}");
+
+                Console.WriteLine($"Flags: {commandParser.Flags.Count}");
+                i = 1; foreach (var flag in commandParser.Flags)
+                    Console.WriteLine($"{i++}: {flag}");
             }
             catch (ArgumentException e)
             {
