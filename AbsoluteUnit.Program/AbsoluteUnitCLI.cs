da79559d6fe11df680d66fc10fae3ce668049f83
andr-be <andr-be@github.com>
2024-06-10 16:03:42 +0100

    removed weird debug methods now that I have proper ToString() support on my custom classes.

--- AbsoluteUnit.Program/AbsoluteUnitCLI.cs
+++ AbsoluteUnit.Program/AbsoluteUnitCLI.cs
@@ -6,62 +6,14 @@ namespace AbsoluteUnit
     {
         static void Main(string[] args)
         {
-            DebugProgram(args);
-        }
+            var commandGroup = new CommandParser(args).CommandGroup;
+            Console.WriteLine(commandGroup);
 
-        private static void DebugProgram(string[] args)
-        {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("No arguments provided.");
-                return;
-            }
-            try
-            {
-                CommandParser commandParser = new(args);
+            var measurementGroup = new MeasurementParser(commandGroup.CommandArguments[0]).MeasurementGroup;
+            Console.WriteLine(measurementGroup);
 
-                Console.WriteLine($"Command: {commandParser.CommandType}\n");
-                int i = 1;
-                foreach (var arg in commandParser.CommandArguments)
-                {
-                    Console.WriteLine($"{i++}: {arg}");
-                    try
-                    {
-                        MeasurementParser parser = new(arg);
-                        DebugPrint(parser);
-                        
-                        var absUnits = new UnitFactory(parser.MeasurementGroup.Units).BuildUnits();
-
-                        Console.WriteLine($"{parser.MeasurementGroup}");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-
-                Console.WriteLine($"Flags: {commandParser.Flags.Count}");
-                i = 1;
-                foreach (var flag in commandParser.Flags)
-                    Console.WriteLine($"{i++}: {flag}");
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-
-        private static void DebugPrint(MeasurementParser parser)
-        {
-            Console.WriteLine($"qty: {parser.MeasurementGroup.Quantity}");
-            Console.WriteLine($"exp: {parser.MeasurementGroup.Exponent}");
-            Console.WriteLine($"units:");
-            foreach (var unitGroup in parser.MeasurementGroup.Units)
-            {
-                Console.WriteLine($"  div: {unitGroup.Operation}");
-                Console.WriteLine($"  sym: {unitGroup.UnitSymbol}");
-                Console.WriteLine($"  exp: {unitGroup.Exponent}\n");
-            }
+            var units = new UnitFactory(measurementGroup.Units).BuildUnits();
+            foreach (var unit in units) Console.WriteLine(unit);
         }
     }
 }
