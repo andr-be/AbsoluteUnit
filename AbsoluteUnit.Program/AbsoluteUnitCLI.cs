5fdf9d8a59cb340354beda1e220025604d618bde
andr-be <andr-be@github.com>
2024-06-10 16:16:24 +0100

    minor change to CLI frontend (now creates an AbsMeasurement)

--- AbsoluteUnit.Program/AbsoluteUnitCLI.cs
+++ AbsoluteUnit.Program/AbsoluteUnitCLI.cs
@@ -9,11 +9,9 @@ namespace AbsoluteUnit
             var commandGroup = new CommandParser(args).CommandGroup;
             Console.WriteLine(commandGroup);
 
-            var measurementGroup = new MeasurementParser(commandGroup.CommandArguments[0]).MeasurementGroup;
+            var measurementGroup = new MeasurementParser(commandGroup.CommandArguments[0]).ProcessMeasurement();
             Console.WriteLine(measurementGroup);
 
-            var units = new UnitFactory(measurementGroup.Units).BuildUnits();
-            foreach (var unit in units) Console.WriteLine(unit);
         }
     }
 }
