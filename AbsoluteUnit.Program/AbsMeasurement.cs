6ee387a3f7bdde2934c4e352f9175551dd167a2e
andr-be <andr-be@github.com>
2024-06-12 23:36:51 +0100

    refactored AbsMeasurement to make it simpler

--- AbsoluteUnit.Program/AbsMeasurement.cs
+++ AbsoluteUnit.Program/AbsMeasurement.cs
@@ -6,11 +6,8 @@ public class AbsUnit(IUnit unit, int exponent, SIPrefix prefix)
     public int Exponent { get; set; } = exponent;
     public SIPrefix Prefix { get; set; } = prefix;
 
-    public override string ToString()
-    {
-        string exponent = (Exponent != 1) ? "^" + Exponent : "";
-        return $"{Prefix}{Unit.Symbol}{exponent}";
-    }
+    public override string ToString() => 
+        $"{Prefix}{Unit.Symbol}{((Exponent != 1) ? "^" + Exponent : "")}";
 }
 
 public class AbsMeasurement
@@ -19,19 +16,18 @@ public class AbsMeasurement
     public double Quantity { get; set; }
     public int Exponent { get; set; }
 
+    public AbsMeasurement(List<AbsUnit>? units = null, double quantity = 0.0, int exponent = 1)
+    {
+        Units = units ?? [];
+        Quantity = quantity;
+        Exponent = exponent;
+    }
+
     public AbsMeasurement(AbsUnit unit, double quantity = 0.0, int exponent = 1)
+        : this([unit], quantity, exponent)
     {
-        Units = [unit];
-        Quantity = quantity;
-        Exponent = exponent;
     }
 
-    public AbsMeasurement(List<AbsUnit> units, double quantity = 0.0, int exponent = 1)
-    {
-        Units = units;
-        Quantity = quantity;
-        Exponent = exponent;
-    }
-
-    public override string ToString() => $"{Quantity}e{Exponent} {string.Join(".", Units)}";
+    public override string ToString() => 
+        $"{Quantity}e{Exponent} {string.Join(".", Units)}";
 }
