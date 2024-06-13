11e047d1a5124b4a0e9dcc603594e94a7170f31c
andr-be <andr-be@github.com>
2024-06-13 16:13:57 +0100

    refactored Convert.cs to make more use of functional syntax; converted it to a static class.

--- AbsoluteUnit.Program/Convert.cs
+++ AbsoluteUnit.Program/Convert.cs
@@ -43,55 +43,50 @@ public class Convert : ICommand
         $"{CommandGroup.CommandType}:\t{FromUnit} -> {string.Join(".", ToUnit.Units)} == {Execute()}";
 }
 
-public class MeasurementConverter
+public static class MeasurementConverter
 {
-    public static AbsMeasurement ToBaseMeasurement(AbsMeasurement from)
-    {
-        var newUnits = GetBaseUnits(from.Units);
-        var newQuantity = QuantityConversion(from.Units, from.Quantity);
-        
-        return new(newUnits, newQuantity, from.Exponent);
-    }
-
-    private static double QuantityConversion(List<AbsUnit> units, double quantity)
-    {
-        var newQuantity = quantity;
-
-        foreach (var unit in units)
-            newQuantity *= unit.ConversionToBase();
-
-        return newQuantity;
-    }
-
-    private static List<AbsUnit>? GetBaseUnits(List<AbsUnit> units)
-    {
-        List<AbsUnit> convertedUnits = [];
-
-        foreach (var unit in units)
-            convertedUnits.Add(ToBaseUnit(unit));
-
-        return convertedUnits;
-    }
-
-    public static AbsUnit ToBaseUnit(AbsUnit from) => 
+    public static AbsMeasurement ToBaseMeasurement(AbsMeasurement from) =>
+        new(ToBaseUnits(from.Units), ToBaseQuantity(from.Units, from.Quantity), from.Exponent);
+    
+    public static AbsUnit ToBaseUnit(AbsUnit from) =>
         new(BaseConversion(from.Unit.Unit), from.Exponent, from.Prefix);
+
+    private static double ToBaseQuantity(List<AbsUnit> units, double quantity) => 
+        units.Select(u => u.ConversionToBase()).Aggregate((x, y) => x * y);
+
+    private static List<AbsUnit>? ToBaseUnits(List<AbsUnit> units) => 
+        units.Select(ToBaseUnit).ToList();
+
+    public static bool ValidConversion(AbsMeasurement from, AbsMeasurement to) =>
+        ToBaseMeasurement(new AbsMeasurement(from.Units))
+        .Equals(ToBaseMeasurement(new AbsMeasurement(to.Units)));
 
     public static SIBase BaseConversion(object unit) => unit switch
     {
+        // Base SI Units convert to themselves!
+        SIBase.Units.Meter => new(SIBase.Units.Meter),
+        SIBase.Units.Gram => new(SIBase.Units.Gram),
+        SIBase.Units.Second => new(SIBase.Units.Second),
+        SIBase.Units.Ampere => new(SIBase.Units.Ampere),
+        SIBase.Units.Kelvin => new(SIBase.Units.Kelvin),
+        SIBase.Units.Mole => new(SIBase.Units.Mole),
+        SIBase.Units.Candela => new(SIBase.Units.Candela),
+        
+        // US Customary Length
         USCustomary.Units.Mil => new(SIBase.Units.Meter),
         USCustomary.Units.Inch => new(SIBase.Units.Meter),
         USCustomary.Units.Feet => new(SIBase.Units.Meter),
         USCustomary.Units.Yards => new(SIBase.Units.Meter),
         USCustomary.Units.Miles => new(SIBase.Units.Meter),
-
+        // US Customary Mass
         USCustomary.Units.Ounce => new(SIBase.Units.Gram),
         USCustomary.Units.Pound => new(SIBase.Units.Gram),
         USCustomary.Units.Ton => new(SIBase.Units.Gram),
-
+        // US Customary Volume
         USCustomary.Units.FluidOunce => new(SIBase.Units.Meter),
         USCustomary.Units.Pint => new(SIBase.Units.Meter),
         USCustomary.Units.Gallon => new(SIBase.Units.Meter),
-
+        // US Customary Temperature
         USCustomary.Units.Fahrenheit => new(SIBase.Units.Kelvin),
     };
 }
