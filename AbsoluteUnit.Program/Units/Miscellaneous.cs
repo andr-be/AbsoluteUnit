0d4f8a1705b39b097fee5737686fa8f28f0d9cb3
andr-be <andr-be@github.com>
2024-08-12 17:29:09 +0100

    Implemented Miscellaneous units; so far just time based ones.

--- AbsoluteUnit.Program/Units/Miscellaneous.cs
+++ AbsoluteUnit.Program/Units/Miscellaneous.cs
@@ -2,26 +2,82 @@
 
 namespace AbsoluteUnit.Program.Units;
 
-public class Miscellaneous : IUnitType
+public class Miscellaneous(Miscellaneous.Units unit) : IUnitType
 {
-    public string Symbol => throw new NotImplementedException();
-
-    public object Unit => throw new NotImplementedException();
-
-    public double FromBase(double value)
+    public enum Units
     {
-        throw new NotImplementedException();
+        // Time
+        Year,
+        Month,
+        Day,
+        Hour,
+        Minute,
     }
 
-    public double ToBase(double value)
+    public string Symbol => Unit switch
     {
-        throw new NotImplementedException();
-    }
+        Units.Year => "yr",
+        Units.Month => "mth",
+        Units.Day => "day",
+        Units.Hour => "h",
+        Units.Minute => "min",
+        _ => throw new NotImplementedException($"{Unit} has no Symbol implementation!")
+    };
 
-    public List<Unit> ExpressInBaseUnits()
+    public object Unit { get; init; } = unit;
+
+    public double FromBase(double value) => Unit switch
     {
-        throw new NotImplementedException();
-    }
+        _ => value / Conversion[unit],
+    };
 
-    public static readonly Dictionary<string, object> ValidUnitStrings = [];
+    public double ToBase(double value) => Unit switch
+    {
+        _ => value * Conversion[unit],
+    };
+
+    public List<Unit> ExpressInBaseUnits() => (Units)Unit switch
+    {
+        Units.Year or
+        Units.Month or
+        Units.Day or
+        Units.Hour or
+        Units.Minute => [SIBase.Second()],
+
+        _ => throw new NotImplementedException($"No base unit conversion for {Unit}")
+    };
+
+    public static readonly Dictionary<string, object> ValidUnitStrings = new()
+    {
+        { "yr", new Miscellaneous(Units.Year) },
+        { "yrs", new Miscellaneous(Units.Year) },
+        { "year", new Miscellaneous(Units.Year) },
+        { "years", new Miscellaneous(Units.Year) },
+        
+        { "mth", new Miscellaneous(Units.Month) },
+        { "mths", new Miscellaneous(Units.Month) },
+        { "month", new Miscellaneous(Units.Month) },
+        { "months", new Miscellaneous(Units.Month) },
+        
+        { "day", new Miscellaneous(Units.Day) },
+        { "days", new Miscellaneous(Units.Day) },
+        
+        { "h", new Miscellaneous(Units.Hour) },
+        { "hr", new Miscellaneous(Units.Hour) },
+        { "hrs", new Miscellaneous(Units.Hour) },
+        { "hour", new Miscellaneous(Units.Hour) },
+        { "hours", new Miscellaneous(Units.Hour) },
+        
+        { "min", new Miscellaneous(Units.Minute) },
+        { "mins", new Miscellaneous(Units.Minute) },
+    };
+
+    static readonly Dictionary<Units, double> Conversion = new()
+    {
+        { Units.Year,   3.15360000e7 },
+        { Units.Month,  2.62800288e6 },
+        { Units.Day,    86400 },
+        { Units.Hour,   3600 },
+        { Units.Minute, 60 },
+    };
 }
