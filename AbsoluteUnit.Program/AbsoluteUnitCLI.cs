c72a828edc5de7e69cf76d1e4f006bbcfd8d207b
andr-be <andr-be@github.com>
2024-08-16 15:45:24 +0100

    changed how Main is running atm; it doesn't use any of the arguments provided but instead runs a full integration test from a static string array. {PS: legal unit conversion validation is completely broken}

--- AbsoluteUnit.Program/AbsoluteUnitCLI.cs
+++ AbsoluteUnit.Program/AbsoluteUnitCLI.cs
@@ -1,6 +1,7 @@
 ﻿using AbsoluteUnit.Program;
 using AbsoluteUnit.Program.Factories;
 using AbsoluteUnit.Program.Commands;
+using System;
 
 namespace AbsoluteUnit
 {
@@ -8,16 +9,27 @@ namespace AbsoluteUnit
     {
         public static void Main(string[] args)
         {
+            foreach (var test in testArguments)
+            {
+                try
+                {
+                    Run(test);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\n" + e.ToString() + "\n");
+                }
+            }
+        }
+
+        public static void Run(string[] args)
+        {
             var commandFactory = new CommandFactory(args);
             var commandGroup = commandFactory.ParseArguments();
 
             // intentionally left long to remind you to fix this mess
             var commandRunner = new Runner(commandGroup)
-                .ParseCommandArguments(new MeasurementParser(
-                    ParserFactory.CreateUnitGroupParser(), 
-                    ParserFactory.CreateUnitFactory()
-                    )
-                );
+                .ParseCommandArguments(ParserFactory.CreateMeasurementParser());
 
             var result = commandRunner.Run();
             
@@ -26,6 +38,14 @@ namespace AbsoluteUnit
             Console.WriteLine(commandRunner.Command);
             Console.WriteLine(result);
         }
+
+        private static string[][] testArguments =
+        [
+            ["--convert", "0.2330 in/µs", "m/s", "-sig", "3"],
+            ["--convert", "20 m/s", "km/h"],
+            ["--express", "100J"],
+            ["--convert", "10 days", "hours"],
+        ];
     }
 
     public static class ParserFactory
@@ -33,5 +53,7 @@ namespace AbsoluteUnit
         public static IUnitGroupParser CreateUnitGroupParser() => new UnitGroupParser();
 
         public static IUnitFactory CreateUnitFactory() => new UnitFactory();
+
+        public static MeasurementParser CreateMeasurementParser() => new(CreateUnitGroupParser(), CreateUnitFactory());
     }
 }
