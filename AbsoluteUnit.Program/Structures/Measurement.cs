50f9993dba10d0bf9cf5f08580d5094f9e8a740d
andr-be <andr-be@github.com>
2024-08-17 17:22:48 +0100

    explicated the ConvertTo() logic quite significantly; this should make it significantly easier to track down the pernicious conversion quantity bug!

--- AbsoluteUnit.Program/Structures/Measurement.cs
+++ AbsoluteUnit.Program/Structures/Measurement.cs
@@ -4,19 +4,49 @@ namespace AbsoluteUnit.Program.Structures;
 public class Measurement(
     List<Unit>? units = null,
     double quantity = 1.0,
-    int exponent = 1)
+    int exponent = 0)
 {
     public List<Unit> Units { get; set; } = units ?? [];
     public double Quantity { get; set; } = quantity;
     public int Exponent { get; set; } = exponent;
 
-    public Measurement(Unit unit, double quantity = 0.0, int exponent = 1)
+    public Measurement(Unit unit, double quantity = 0.0, int exponent = 0)
         : this([unit], quantity, exponent)
     {
     }
 
-    public Measurement ConvertTo(Measurement target) =>
-        new(target.Units, Quantity * QuantityConversionFactor(target), Exponent);
+    public Measurement ConvertTo(Measurement target, bool standardForm=false)
+    {
+        var newUnits = target.Units;
+        var normalisedQuantity = Quantity * Math.Pow(10, Exponent);
+        var convertedQuantity = CalculateNewQuantity(target, normalisedQuantity);
+
+        (double newQuantity, int newExponent) = standardForm
+            ? CalculateNewExponents(convertedQuantity)
+            : (convertedQuantity, 0);
+        
+        return new Measurement(newUnits, newQuantity, newExponent);
+    }
+
+    double CalculateNewQuantity(Measurement target, double quantity) => quantity * QuantityConversionFactor(target);
+
+    static (double newQuantity, int newExponent) CalculateNewExponents(double convertedQuantity)
+    {
+        double factorOfTen = 0.0;
+        double standardFormTracker = 0.0;
+
+        while (convertedQuantity > standardFormTracker)
+        {
+            standardFormTracker = Math.Pow(10, factorOfTen);
+            factorOfTen += 1;
+        }
+
+        return
+        (
+            newQuantity: convertedQuantity / (standardFormTracker / 10),
+            newExponent: (int)factorOfTen
+        );
+    }
 
     public double QuantityConversionFactor(Measurement target) =>
          Units.AggregateConversionFactors() / target.Units.AggregateConversionFactors();
