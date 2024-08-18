0eebf4662fe49fa2d11db89a7bca607b777c36b7
andr-be <andr-be@github.com>
2024-08-18 21:49:35 +0100

    Added Standard Form handling to Convert command.

--- AbsoluteUnit.Program/Commands/Convert.cs
+++ AbsoluteUnit.Program/Commands/Convert.cs
@@ -9,14 +9,19 @@ public class Convert : ICommand
     public double ConversionFactor { get; }
 
     private CommandGroup CommandGroup { get; }
+    private bool StandardForm { get; } = false;
 
     public Convert(CommandGroup commandGroup, IMeasurementParser measurementParser)
     {
         CommandGroup = commandGroup;
 
-        FromUnit = measurementParser.ProcessMeasurement(commandGroup.CommandArguments[0]);
+        FromUnit = measurementParser
+            .ProcessMeasurement(commandGroup.CommandArguments[0]);
 
-        ToUnit = measurementParser.ProcessMeasurement(commandGroup.CommandArguments[1], unitOnly: true);
+        ToUnit = measurementParser
+            .ProcessMeasurement(commandGroup.CommandArguments[1], unitOnly: true);
+        
+        StandardForm = commandGroup.Flags.ContainsKey(Flag.StandardForm);
 
         if (FromUnit.IsValidConversion(ToUnit))
             ConversionFactor = FromUnit.QuantityConversionFactor(ToUnit);
@@ -24,7 +29,7 @@ public class Convert : ICommand
         else throw new ArgumentException($"Cannot convert from {FromUnit} to {ToUnit}");
     }
 
-    public Measurement Run() => FromUnit.ConvertTo(ToUnit);
+    public Measurement Run() => FromUnit.ConvertTo(ToUnit, StandardForm);
 
     public override string ToString() =>
         $"{CommandGroup.CommandType}:\tFrom: {FromUnit}\tTo: {string.Join(".", ToUnit.Units)}";
